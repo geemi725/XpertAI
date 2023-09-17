7341a30b03a4abbb8102962c7046801d7ad14700
geemi725 <gwellawatte@gmail.com>
2023-09-17 21:32:07 +0200

    uncommented

--- app.py
+++ app.py
@@ -2,11 +2,11 @@ import streamlit as st
 import pandas as pd
 import os
 from PIL import Image
-#from Ipython.core.display import HTML
+from Ipython.core.display import HTML
 from dotenv import load_dotenv
 
-#load_dotenv()
-#ss = st.session_state
+load_dotenv()
+ss = st.session_state
 
 st.title("Expert AI")
 st.write('''### Extract structure-function relationships from your data!''')
@@ -23,7 +23,7 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-'''def on_api_key_change():
+def on_api_key_change():
     #api_key = ss.get('api_key') or os.getenv('OPENAI_API_KEY')
     api_key = os.getenv('OPENAI_API_KEY')
     os.environ["OPENAI_API_KEY"] = api_key
@@ -36,4 +36,4 @@ with st.sidebar:
 
     # Input OpenAI api key
     st.markdown('Input your OpenAI API key.')
-    st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")'''
+    st.text_input('OpenAI API key', type='password', key='api_key', on_change=on_api_key_change, label_visibility="collapsed")
