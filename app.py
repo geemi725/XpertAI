e836128d47fcd99096127379b61b5777f6641eaf
geemi725 <gwellawatte@gmail.com>
2023-09-17 20:08:18 +0200

    gitignore updated

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import os
 from PIL import Image
-#from IPython.core.display import HTML
+from IPython.core.display import HTML
 #from dotenv import load_dotenv
 
 #load_dotenv()
