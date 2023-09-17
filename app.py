015d6c2fbe3dec39cca16a1fe465a3adf8d8a36d
Geemi Wellawatte <49410838+geemi725@users.noreply.github.com>
2023-09-17 23:26:55 +0000

    initiate agent wit api

--- app.py
+++ app.py
@@ -4,11 +4,6 @@ import pandas as pd
 import os
 from PIL import Image
 from dotenv import load_dotenv
-from expert_ai.tools import tools
-from expert_ai.agent import ExpertAI
-agent = ExpertAI(verbose=True)
-tools = tools.get_tools()
-
 
 load_dotenv()
 ss = st.session_state
@@ -35,6 +30,11 @@ def on_api_key_change():
     api_key = ss.get('api_key') or os.getenv('OPENAI_API_KEY')
     #api_key = os.getenv('OPENAI_API_KEY')
     os.environ["OPENAI_API_KEY"] = api_key
+    from expert_ai.tools import tools
+    from expert_ai.agent import ExpertAI
+    global agent
+    agent = ExpertAI(verbose=True)
+    tools = tools.get_tools()
 
 
 # sidebar
@@ -46,9 +46,12 @@ with st.sidebar:
     st.markdown('Input your OpenAI API key.')
     st.text_input('OpenAI API key', type='password', key='api_key',  
                   on_change=on_api_key_change, label_visibility="visible")
-    st.markdown('Upload your files here!')
-    input_file = st.file_uploader("Upload dataset")
-    lit_dir = st.file_uploader("Upload your literature library here", 
+    
+    
+    
+    st.markdown('Upload your input files')
+    input_file = st.file_uploader("Upload dataset here:")
+    lit_dir = st.file_uploader("Upload your literature library here:", 
                                accept_multiple_files=True)
     
 
@@ -56,6 +59,7 @@ if prompt := st.chat_input():
     st.chat_message("user").write(prompt)
     if input_file is not None:
         prompt += f'datapath:{input_file}'
+    
     with st.chat_message("assistant"):
         response = agent.run(query=prompt)
         st.write(response)
