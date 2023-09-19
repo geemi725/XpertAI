ebf7c5f3a9d8eb9ffd78876e0af63512cf6c48ee
geemi725 <gwellawatte@gmail.com>
2023-09-19 13:15:43 +0200

    tabs added

--- app.py
+++ app.py
@@ -9,18 +9,8 @@ from io import StringIO
 import pandas as pd
 from langchain.callbacks import StreamlitCallbackHandler
 from dotenv import load_dotenv
-
-
 load_dotenv()
 ss = st.session_state
-
-logo = Image.open('assets/logo.png')
-st.image(logo)
-#st.title("Xpert AI")
-st.write('''### Extract structure-function relationships from your data!
-
-This is a simple app which helps you to extract human interpretable relationships
-in your dataset. ''')
 
 # Set width of sidebar
 st.markdown(
@@ -34,7 +24,6 @@ st.markdown(
     unsafe_allow_html=True,
 )
 
-
 def on_api_key_change():
     api_key = ss.get('api_key') or os.getenv('OPENAI_API_KEY')
     #api_key = os.getenv('OPENAI_API_KEY')
@@ -44,48 +33,57 @@ def on_api_key_change():
     global agent    
     agent = ExpertAI(verbose=True)
 
+## Header section
+logo = Image.open('assets/logo.png')
+st.image(logo)
+#st.title("Xpert AI")
+st.write('''### Extract structure-function relationships from your data!
 
-# sidebar
-#with st.sidebar:
-#logo = Image.open('assets/logo.png')
-#st.image(logo)
+This is a simple app which helps you to extract human interpretable relationships
+in your dataset. ''')
+         
+tab1, tab2, tab3 = st.tabs(['Setup', 'Explanations', 'Q&A'])
 
-# Input OpenAI api key
-st.markdown('Input your OpenAI API key.')
-api_key = st.text_input('OpenAI API key', type='password', key='api_key',  
-                on_change=on_api_key_change, label_visibility= "hidden")   
+with tab1:
+    # Input OpenAI api key
+    st.markdown('### Input your OpenAI API key.')
+    api_key = st.text_input('OpenAI API key', type='password', key='api_key',  
+                    on_change=on_api_key_change, label_visibility= "hidden")   
 
 
-st.markdown('### Upload your input dataset')
-input_file = st.file_uploader("Input to extract relationships from (must have .csv extention):")
+    st.markdown('### Upload your input dataset')
+    input_file = st.file_uploader("Input to extract relationships from (must have .csv extention):")
 
-st.markdown('### Set up XAI workflow')
-mode_type =  st.radio("Select the model type",
-                        ["Regressor", "Classifier"],
-                        captions= ["For predicting continuous values", "For predicting discreet labels"])
-label = st.text_input("Target label",
-                        help='Label you are trying to predict. Should match the label in the dataset.')
-XAI_tool =  st.radio("What XAI method would you like to try?",
-                        ["SHAP", "LIME","Both"])
-top_k =   st.slider('Number of top features for the XAI analysis', 0, 10, 1) 
+    st.markdown('### Set up XAI workflow')
+    mode_type =  st.radio("Select the model type",
+                            ["Regressor", "Classifier"],
+                            captions= ["For predicting continuous values", "For predicting discreet labels"])
+    label = st.text_input("Target label",
+                            help='Label you are trying to predict. Should match the label in the dataset.')
+    XAI_tool =  st.radio("What XAI method would you like to try?",
+                            ["SHAP", "LIME","Both"])
+    top_k =   st.slider('Number of top features for the XAI analysis', 0, 10, 1) 
 
-st.markdown("### Select method of literature retrieval \nYou can either upload a literature dataset or scrape arxiv.org. If you don't provide literature, you will receive an explanation based on XAI tools.")
-#lit_dir = st.file_uploader("Upload your literature library here (Optional):", 
-#                           accept_multiple_files=True)
-arxiv_keywords = st.text_input("Keywords for arxiv scraping:",
-                                help='Keywords to scrape arxiv.org')
-max_papers = st.number_input("Number of papers", key=int, value=10,
-                        help='Maximum number of papers to download from arxiv.org')
+    st.markdown("### Select method of literature retrieval \nYou can either upload a literature dataset or scrape arxiv.org. If you don't provide literature, you will receive an explanation based on XAI tools.")
+    #lit_dir = st.file_uploader("Upload your literature library here (Optional):", 
+    #                           accept_multiple_files=True)
+    arxiv_keywords = st.text_input("Keywords for arxiv scraping:",
+                                    help='Keywords to scrape arxiv.org')
+    max_papers = st.number_input("Number of papers", key=int, value=10,
+                            help='Maximum number of papers to download from arxiv.org')
 
-observation = st.text_input("What is the property you'd like explained?",
-                                help='e.g: Size of pore limiting diameter')
+    observation = st.text_input("What is the property you'd like explained?",
+                                    help='e.g: Size of pore limiting diameter')
 
-button = st.button("Generate Explanation")
+    button = st.button("Generate Explanation")
 
-if api_key:
-    from expert_ai.tools.explain_model import get_modelsummary
-    from expert_ai.tools.scrape_arxiv import scrape_arxiv
-    from expert_ai.tools.generate_nle import gen_nle
+
+with tab2:
+    if api_key:
+        from expert_ai.tools.explain_model import get_modelsummary
+        from expert_ai.tools.scrape_arxiv import scrape_arxiv
+        from expert_ai.tools.generate_nle import gen_nle
+
     if button:
 
         df_init = pd.read_csv(input_file,header=0)
@@ -127,21 +125,30 @@ if api_key:
             st.write("## The structure function relationship can be explained as below:\n", 
                     nle)
 
+# sidebar
+#with st.sidebar:
+#logo = Image.open('assets/logo.png')
+#st.image(logo)
 
-print(st.session_state)
-# Agent execution
-st.write("## You can retrieve more information from literature using the Q&A chat tool!")
-prompt = st.chat_input("Ask me")
-if prompt:
-    st.chat_message("user").write(prompt)
-    with st.chat_message("assistant"):
-        response = agent.run(query=prompt)
-        st.write(response)
-        '''st_callback = StreamlitCallbackHandler(
-            st.container(),
-            max_thought_containers = 4,
-            collapse_completed_thoughts = False,
-            output_placeholder=st.session_state
-        )
-    '''
-    
+
+
+
+
+with tab3:
+    print(st.session_state)
+    # Agent execution
+    st.write("## You can retrieve more information from literature using the Q&A chat tool!")
+    prompt = st.chat_input("Ask me")
+    if prompt:
+        st.chat_message("user").write(prompt)
+        with st.chat_message("assistant"):
+            response = agent.run(query=prompt)
+            st.write(response)
+            '''st_callback = StreamlitCallbackHandler(
+                st.container(),
+                max_thought_containers = 4,
+                collapse_completed_thoughts = False,
+                output_placeholder=st.session_state
+            )
+        '''
+        
