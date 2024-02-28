063ca5ce27204fd2a573c7644e6249f01e061052
geemi725 <geemi.wellawatte@epfl.ch>
2024-02-28 11:13:27 +0100

    black formatting

--- xpertai/tools/scrape_arxiv.py
+++ xpertai/tools/scrape_arxiv.py
@@ -6,7 +6,7 @@ from .utils import *
 
 
 def scrape_arxiv(arg_dict):
-    '''Takes a  dictionary as input in the form:
+    """Takes a  dictionary as input in the form:
     {"key_words":<key words to search arxiv>,
     "max_papers":<maximum number of papers to download>,
     "save_dir": <path to save downloaded papers>
@@ -20,29 +20,25 @@ def scrape_arxiv(arg_dict):
 
     returns:
         scrapes ArXiv for papers by given key words and download
-    '''
+    """
 
     # arg_dict = json.loads(json_request)
     for k, val in arg_dict.items():
         globals()[k] = val
 
-    save_dir = './data/downloads/'
+    save_dir = "./data/downloads/"
     if os.path.exists(save_dir):
         shutil.rmtree(save_dir)
 
     os.mkdir(save_dir)
 
     search = arxiv.Search(
-        query=key_words,
-        max_results=max_papers,
-        sort_by=arxiv.SortCriterion.Relevance
+        query=key_words, max_results=max_papers, sort_by=arxiv.SortCriterion.Relevance
     )
 
     for result in search.results():
-        title = '_'.join(result.title.split(' '))
+        title = "_".join(result.title.split(" "))
         cleaned = re.sub(r"[^a-zA-Z0-9.]|(?<!\d)\.|\.(?!\d)", "_", title)
         result.download_pdf(dirpath=save_dir, filename=f"{cleaned}.pdf")
         # update vectordb
-        vector_db(lit_file=f'{save_dir}{cleaned}.pdf',
-                  clean=False,
-                  try_meta_data=True)
+        vector_db(lit_file=f"{save_dir}{cleaned}.pdf", clean=False, try_meta_data=True)
